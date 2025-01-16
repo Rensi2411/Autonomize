3033080d5969f28311ec5288a47c1740709cf88c
Rensi <rensidudhat2411@gmail.com>
2025-01-16 21:23:26 +0530

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
-require('dotenv').config();
-const app = require('./app');
-const sequelize = require('./utils/db');
-
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-sequelize.sync({ force: false }).then(() => {
-  console.log('Database connected!');
-  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the GitHub User API!');
+});
+
+// Add other API routes here
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
